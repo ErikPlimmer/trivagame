cf7ff964f6022e64ce6232cc17001e9dbb285a4d
Erik Plimmer <Elp013@Gmail.com>
2018-01-01 15:27:56 -0500

    update1/1

---
+++ assets/app.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+
+
+
+
+
+function startGame() {
+
+	// document.querySelector(".q1").innerHTML = "question here" +  + "<br><br><br><br>";
+	// document.querySelector(".q2").innerHTML = "question here " +  + "<br><br><br><br>";
+	// document.querySelector(".q3").innerHTML = "question here " +  + "<br><br><br><br>";
+	// document.querySelector(".q4").innerHTML = "question here " +  + "<br><br><br><br>";
+	// document.querySelector(".q5").innerHTML = "question here" +  + "<br><br><br><br>";
+	// document.querySelector("#button").value = "Play again" + " ";
+
+
+
+}
