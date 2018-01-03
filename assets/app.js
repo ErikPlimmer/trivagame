862acd9f3a2a728445dd6291266d018510d0cca7
Erik Plimmer <Elp013@Gmail.com>
2018-01-03 17:33:33 -0500

    update1/3

--- assets/app.js
+++ assets/app.js
@@ -6,14 +6,14 @@ var incomplete = 0;
 
 var timer = 0;
 
-var number = 120;
+var number = 60;
 
 var intervalId;
 
 	function startGame() {
 		$("#timer").text(number);
 		run();
-
+	document.querySelector("#button").value = "Play again";
 	}
 
     function run() {
@@ -31,60 +31,67 @@ var intervalId;
 
       //  Once number hits zero...
       if (number === 0) {
+	
+		clearInterval(intervalId);
 
         endGame();
       }
     }
 
-    //  The stop function
-    function stop() {
-
-	}
 
 
 
 function endGame() {
-
+	// Checking if answered correctly or incorrectly 
 	var q1 = ($("input[name=q1]:checked").val()!="heart");
 	if (q1 === true) {
 		incorrect+=1;
 	}	else {
 		correct+=1;
 	}
-
+	// Checking if answered correctly or incorrectly 
 	var q2 = ($("input[name=q2]:checked").val()!="chester");
 	if (q2 === true) {
 		incorrect+=1;
 	}	else {
 		correct+=1;
 	}
-
+	// Checking if answered correctly or incorrectly 
 	var q3 = ($("input[name=q3]:checked").val()!="true");
 	if (q3 === true) {
 		incorrect+=1;
 	}	else {
 		correct+=1;
 	}
-
+	// Checking if answered correctly or incorrectly 
 	var q4 = ($("input[name=q4]:checked").val()!="1");
 	if (q4 === true) {
 		incorrect+=1;
 	}	else {
 		correct+=1;
 	}
-
+	// Checking if answered correctly or incorrectly 
 	var q5 = ($("input[name=q5]:checked").val()!="smallwrld");
 	if (q5 === true) {
 		incorrect+=1;
 	}	else {
 		correct+=1;
 	}
-
+	// Clearing timer
 	clearInterval(intervalId);
-	document.querySelector(".q1").innerHTML = "Correct answers:" + correct + "<br>";
-	document.querySelector(".q2").innerHTML = "Incorrect answers: " + incorrect + "<br>";
+	// Replace html with correct and incorrect answers
+	document.querySelector(".q1").innerHTML = "Correct answers:" + correct;
+	document.querySelector(".q2").innerHTML = "Incorrect answers: " + incorrect;
 	document.querySelector(".q3").innerHTML = " " + "<br>";
 	document.querySelector(".q4").innerHTML = " " + "<br>";
 	document.querySelector(".q5").innerHTML = " " + "<br>";
-	document.querySelector("#button").value = "Play again" ;
+
 }
+
+//===========================================================================================
+// Unfinished goals 
+// make "play again" button reset game 
+// stylize questions css
+// stylize the endgame screen 
+// change color of timer text when nearing timeout 
+// display number of unanswered questions at endgame
